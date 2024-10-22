05d7ca56d35fd5af623bd2025895c40c48e731f2
Your Name <you@example.com>
2024-10-22 11:53:10 +0530

    mongo db connection and error handler

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const connectDb = require("./config/dbconnect");
-const errorHandler  = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnections.js");
+const errorHandler  = require("./middlewares/errorHandler.js");
 const cors = require("cors");
+
+//env file config
+const dotenv = require("dotenv");
+dotenv.config();
 
 connectDb();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000 || 4000 || 4900 ||1000;
 
 app.use(express.json());
 app.use(cors()); 
-
 app.get('/',(req,res)=>{
     res.send("working");
 });
