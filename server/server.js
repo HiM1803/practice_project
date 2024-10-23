c3ac36a34233236f5af5dd0f0b34626534a562bc
Your Name <you@example.com>
2024-10-23 11:31:28 +0530

    hbs and server update

--- server/server.js
+++ server/server.js
@@ -10,13 +10,27 @@ dotenv.config();
 connectDb();
 
 const app = express();
-const port = process.env.port || 5000 || 4000 || 4900 ||1000;
+const port = 5000 || 4000 || 4900 ||1000 || 2999 || 1024 || 8080;
 
 app.use(express.json());
 app.use(cors()); 
 app.get('/',(req,res)=>{
     res.send("working");
 });
+
+app.get('/home',(req,res)=>{
+    res.render('home',{
+        username: "xyz",
+        posts: "flana dhimkana"
+    })
+})
+
+app.get('/allusers',(req,res)=>{
+    res.render('allusers',{
+        data:[{name:"abc", age:20},
+            {name:"def", age:19}]
+    })
+})
 
 app.listen(port,()=>{
     console.log(`server running on port http://localhost:${port}`);
